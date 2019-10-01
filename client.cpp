7fbbb2c3d5c0ec5c161afafe1d99a09f6e1cf97b
Joseph Gibson <jkpgibson@gmail.com>
2019-10-01 16:28:50 -0500

    loop

--- client.cpp
+++ client.cpp
@@ -7,6 +7,7 @@
 #include "common.h"
 #include "FIFOreqchannel.h"
 #include <fstream>
+#include <chrono>
 
 using namespace std;
 
@@ -18,9 +19,10 @@ int main(int argc, char *argv[]){
 
     FIFORequestChannel chan ("control", FIFORequestChannel::CLIENT_SIDE);
 
-    //Task 1
+    // Begin Task 1
+    auto start = chrono::steady_clock::now();
     ofstream x_1("x1.csv");
-    for (double i = 0; i <= 59.996; i+= 0.04){
+    for (double i = 0; i <= 59.996; i+= 0.004){
         datamsg ecg_1 = datamsg(1, i, 1);
         datamsg ecg_2 = datamsg(1, i, 2);
         chan.cwrite (&ecg_1, sizeof(ecg_1));
@@ -29,10 +31,14 @@ int main(int argc, char *argv[]){
         char* buf2 = chan.cread ();
 
         if (x_1.is_open()){
-            x_1 << *((double*) buf1) << "," << *((double*) buf2) << endl;
+            x_1 << i << "," << *((double*) buf1) << "," << *((double*) buf2) << endl;
         }
     }
     x_1.close();
+    auto end = chrono::steady_clock::now();
+    int64_t elapsed = chrono::duration_cast<chrono::milliseconds>(end - start).count();
+    cout << elapsed << " milliseconds" << endl;
+    // End Task 1
 
 
     // closing the channel    
